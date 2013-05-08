3488007d977e85406315e86558411df30def07fb
Vladislav Lewin <vlewin@suse.de>
2013-05-08 14:43:52 +0200

    clean up

--- spec/sms_spec.rb
+++ spec/sms_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe SMS do
+describe 'SMS server' do
   before(:each){ Notification::Message.destroy_all }
 
   it "establishes a REDIS connection" do
